91205ea90633e5b547cfd5910fb7c67fe2d8b106
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2023-09-21 17:17:51 -0300

    chore: basic express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World! ");
+import express from "express";
+import cors from "cors";
+import dontenv from "dotenv";
+import router from "@routes/index";
+dontenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
