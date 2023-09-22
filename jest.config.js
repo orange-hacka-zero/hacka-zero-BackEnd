59a14a8f80133274421bcc016a7b72065a5fa6df
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2023-09-22 12:40:52 -0300

    chore: add 'alias' for testing modules

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,29 @@ module.exports = {
   },
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/src/$1",
+
+    "@routes/(.*)": "<rootDir>/src/routes/$1",
+    "@routes": "<rootDir>/src/routes/index.ts",
+
+    "@controllers/(.*)": "<rootDir>/src/controllers/$1",
+    "@controllers": "<rootDir>/src/controllers/index.ts",
+
+    "@services/(.*)": "<rootDir>/src/services/$1",
+    "@services": "<rootDir>/src/services/index.ts",
+
+    "@repositories/(.*)": "<rootDir>/src/repositories/$1",
+    "@repositories": "<rootDir>/src/repositories/index.ts",
+
+    "@utils/(.*)": "<rootDir>/src/utils/$1",
+    "@utils": "<rootDir>/src/utils/index.ts",
+
+    "@config/(.*)": "<rootDir>/src/config/$1",
+    "@config": "<rootDir>/src/config/index.ts",
+
+    "@middlewares/(.*)": "<rootDir>/src/middlewares/$1",
+    "@middlewares": "<rootDir>/src/middlewares/index.ts",
+
+    "@validations/(.*)": "<rootDir>/src/validations/$1",
+    "@validations": "<rootDir>/src/validations/index.ts",
   },
 };
