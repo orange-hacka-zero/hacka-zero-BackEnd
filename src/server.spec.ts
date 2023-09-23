450df54c9d469a43a1b30a9315bcce87b6292ef7
Gu-Parlandim <56051040+Gu-Parlandim@users.noreply.github.com>
2023-09-23 16:04:53 -0300

    test:  the 'create user' route/controller

--- src/server.spec.ts
+++ src/server.spec.ts
@@ -12,3 +12,20 @@ describe("Home Routes testing", function () {
     expect(res.text).toEqual("ok");
   });
 });
+
+describe("User Routes testing", function () {
+  test("responds to POST in /user/new_user", async () => {
+    const res = await request(app).post("/user/new_account");
+
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email: expect.any(String),
+      password: expect.any(String),
+      role: expect.any(String),
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    });
+  });
+});
